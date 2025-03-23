fb19dfc2b81616b41ca6bcb1e4b0ae7fb263c4b8
Akash9363 <aksingh9363@gmail.com>
2025-03-23 19:40:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Tours from './components/Tours';
+import data from './data';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [tours, setTours] = useState(data);
+
+  function removeTour(id){
+    const newTours= tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if (tours.length === 0){
+    return(
+      <div className="refresh">
+          <h2>No Tours Left</h2>
+          <button className='btn-white' onClick={() => setTours(data)}>
+            Refresh
+          </button>
+      </div>
+    )
+  }
+
+  return ( 
+    <div className='App'>
+      <Tours tours={tours} removeTour={removeTour}></Tours>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,67 @@
+const data=[
+    {
+        id:1,
+        name:"Agra",
+        info:"Agra is one of the most populous cities in Uttar Pradesh, and the 24th most populous city in India. Agra is a major tourist destination because of its many Mughal-era buildings, most notably the Taj Mahal, Agra Fort and Fatehpur Sikri, all three of which are UNESCO World Heritage Sites.",
+        image:"agra-travel-guide.jpg",
+        price:"35,758",
+    },
+    {
+        id:2,
+        name:"Jaipur",
+        info:"Jaipur became known as “The Pink City” when, in 1876, Maharaja Ram Singh had most of the buildings painted pink—the color of hospitality—in preparation for a visit by Britain's Queen Victoria. Today, the city is known for its bazaars, forts, temples, palaces, and wildlife sanctuaries.",
+        image:"jaipur.jpg",
+        price:"49,560",
+    },
+    {
+        id:3,
+        name:"Goa",
+        info:"Goa, a tiny emerald on the west coast of India, with its natural Scenic beauty, abundant greenery, attractive beaches, historical temples and churches, colourful feasts and festivals, and above all warm and hospitable people with a rich cultural milieu, has today emerged as an ideal tourist destination worldwide.",
+        image:"Goa.jpg",
+        price:"39,695",
+    },
+    {
+        id:4,
+        name:"Varansi",
+        info:"The land of Varanasi (Kashi) has been the ultimate pilgrimage spot for Hindus for ages. Hindus believe that one who is graced to die on the land of Varanasi would attain salvation and freedom from the cycle of birth and re-birth. Abode of Lord Shiva and Parvati, the origins of Varanasi are yet unknown.",
+        image:"Varanasi.jpg",
+        price:"35,758",
+    },
+    {
+        id:5,
+        name:"Jaisalmer",
+        info:"Jaisalmer stands on a ridge of yellowish sandstone and is crowned by the ancient Jaisalmer Fort. This fort contains a royal palace and several ornate Jain temples. Many of the houses and temples of both the fort and of the town below are built of finely sculptured yellow sandstone.",
+        image:"Jaisalmer.jpg",
+        price:"38,595",
+    },
+    {
+        id:6,
+        name:"Kochi",
+        info:"Kochi also known by its former name Cochin, is a major port city along the Malabar Coast of India bordering the Laccadive Sea. It is part of the district of Ernakulam in the state of Kerala. The city is also commonly referred to as Ernakulam.",
+        image:"kochi.jpeg",
+        price:"52,624",
+    },
+    {
+        id:7,
+        name:"Darjeeling",
+        info:"Darjeeling is situated on a long, narrow mountain ridge of the Sikkim Himalayas that descends abruptly to the bed of the Great Rangit River. On a clear day the city affords a magnificent view of Kanchenjunga (28,169 feet [8,586 metres]), and Mount Everest can be seen from a nearby viewing point.",
+        image:"darjeeling.jpeg",
+        price:"42,936",
+    },
+    {
+        id:8,
+        name:"Nanital",
+        info:"Nainital, founded in 1841, is a popular resort. It is built around the beautiful Naina Lake and is surrounded by forested hills. The town is linked by road with the rail terminus at Kathgodam, to the south.Nainital is known as the Lake District of India, and is centered around the emerald-colored Naini Lake, which is surrounded by hills.",
+        image:"Nanital.jpg",
+        price:"46,684",
+    },
+    {
+        id:9,
+        name:"Delhi",
+        info:"Delhi, city and national capital, and union territory, north-central India. The city of Delhi actually consists of two components: Old Delhi, in the north, the historic city; and New Delhi, in the south, since 1947 the capital of India, built in the first part of the 20th century as the capital of British India.",
+        image:"Delhi.jpeg",
+        price:"44,842",
+    }
+];
+
+export default data;
